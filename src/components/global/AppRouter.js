ba3fdb50cf245c8e7715e7b478ba9f797add7eeb
Jonathan Berney <734492+jberney@users.noreply.github.com>
2025-08-15 00:44:51 -0700

    render siteframe without router for now

--- src/components/global/AppRouter.js
+++ src/components/global/AppRouter.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router';
 import { Siteframe } from './siteframe/Siteframe';
 
 export function AppRouter() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Siteframe />} path="/" />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Siteframe />;
 }
